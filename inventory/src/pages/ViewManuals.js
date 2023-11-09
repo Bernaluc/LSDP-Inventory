395db0ee3d4e2b8ae0bf3b46e12d520d4c58b295
jamesrcramos <jamesrcramos@gmail.com>
2023-11-09 13:52:45 -0500

    Completed ViewManuals page

--- inventory/src/pages/ViewManuals.js
+++ inventory/src/pages/ViewManuals.js
@@ -2,33 +2,29 @@ import React from "react";
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 
-function ViewAreas() {
+function ViewManuals() {
     return (
         <div>
-            <h1>View All Areas</h1>
+            <h1>View All Manuals</h1>
             <Table striped bordered hover>
                 <thead>
                     <tr>
+                        <th>ID</th>
                         <th>Name</th>
-                        <th>Equipment</th>
-                        <th>Notes</th>
+                        <th>Link</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>PUM4350</td>
-                        <td>WT01</td>
-                        <td>Polished Water Silo 1 Discharge Pump</td>
+                        <td>1</td>
+                        <td>WT01 Manual</td>
+                        <td>https://WT01-manual.test.com</td>
                     </tr>
                 </tbody>
             </Table>
-            <Button href='/add-component' variant='secondary' size='lg'>
-                Add New Component
-            </Button>
-
         </div>
     )
 }
 
 
-export default ViewAreas;
+export default ViewManuals;
